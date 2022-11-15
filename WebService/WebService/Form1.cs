ae4a894f75d4948f0ad013f6a2b73150141dcd50
Attila András Antalffy-Zsiros <attila.antalffy-zsiros@stud.uni-corvinus.hu>
2022-11-15 23:00:51 +0100

    7.

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -20,9 +20,16 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             GetExchangeRates();
             dataGridView1.DataSource = Rates;
             chartRateData.DataSource = Rates;
+            //comboBox1.DataSource = Rates;
         }
 
         private void GetExchangeRates()
@@ -31,9 +38,9 @@ namespace WebService
 
             var request = new GetExchangeRatesRequestBody
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -71,7 +78,21 @@ namespace WebService
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
 
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
